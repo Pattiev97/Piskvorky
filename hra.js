607096999efa173ee0c9663f2842a509b10201ba
Patricie Vyhlídalová <patina.vy@gmail.com>
2024-04-08 17:53:40 +0200

    přidán posluchač na 10 políček, funkce-kdo hraje

---
+++ hra.js
@@ -0,0 +1,45 @@
+let currentPlayer = 'circle';
+
+const playerTurn = (e) => {
+  e.target.disabled = true;
+  if (currentPlayer === 'circle') {
+    e.target.classList.add('board__field--circle');
+    currentPlayer = 'cross';
+    document.querySelector('.current-player').src = 'pictures/cross.svg';
+  } else {
+    e.target.classList.add('board__field--cross');
+    currentPlayer = 'circle';
+    document.querySelector('.current-player').src = 'pictures/circle.svg';
+  }
+};
+
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', playerTurn);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', playerTurn);
