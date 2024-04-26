68680957db7a5490f881a308b40b4a58fbbcfe0b
Patricie Vyhlídalová <patina.vy@gmail.com>
2024-04-26 19:02:57 +0200

    přidání autohráče-API

--- hra.js
+++ hra.js
@@ -2,7 +2,7 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 let currentPlayer = 'circle';
 const squares = document.querySelectorAll('.square');
 
-const playerTurn = (e) => {
+const playerTurn = async (e) => {
   e.target.disabled = true;
   if (currentPlayer === 'circle') {
     e.target.classList.add('board__field--circle');
@@ -24,17 +24,53 @@ const playerTurn = (e) => {
     }
   });
 
-  const returnWinner = () => {
+  const returnWinner = async () => {
     const winner = findWinner(playground);
     if (winner === 'o' || winner === 'x') {
       alert(`Vyhrál hráč se symbolem "${winner}".`);
       location.reload();
     } else if (winner === 'tie') {
       alert('Hra skončila nerozhodně.');
+    } else {
+      if (currentPlayer === 'cross') {
+        squares.forEach((squares) => {
+          squares.disabled = true;
+        });
+
+        const response = await fetch(
+          'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              board: playground,
+              player: 'x',
+            }),
+          },
+        );
+
+        const data = await response.json();
+        const { x, y } = data.position;
+        const square = squares[x + y * 10];
+
+        squares.forEach((square) => {
+          if (
+            square.classList.contains('board__field--circle') ||
+            square.classList.contains('board__field--cross')
+          ) {
+            square.disabled = true;
+          } else {
+            square.disabled = false;
+          }
+        });
+
+        square.click();
+      }
     }
   };
-
-  setTimeout(returnWinner, 400);
+  setTimeout(returnWinner, 100);
 };
 
 squares.forEach((square) => {
